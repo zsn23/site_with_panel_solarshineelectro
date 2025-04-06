ae71db349612547de1513c304e3dbb3996fefde1
Muhammad Zeeshan <zeeshanijaz14@gmail.com>
2025-04-06 21:24:09 +0500

    padding or login card fixed

--- app/admin/(auth)/login/page.tsx
+++ app/admin/(auth)/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-2 text-center">
           <div className="flex justify-center mb-4">
